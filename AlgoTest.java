650ff59fc714c19a40fbae575ec6410711c0c0b2
David Young <david@nospam.fake>
2014-11-12 20:07:38 -0800

    Fixed the algorithm. Realized I had it wrong in the original commit. This one takes longer. Have fun!

--- AlgoTest.java
+++ AlgoTest.java
@@ -5,22 +5,28 @@
  */
 public class AlgoTest {
     public static void main(String[] args) {
-		long start = System.currentTimeMillis();
+    	long start = System.currentTimeMillis();
 
         for(int num = 1; num < 20; num++) {
             System.out.println("f(" + num + ", " + num + ") = " + f(num, num));
             System.out.println("f(" + num + ", " + (num+1) + ") = " + f(num, num + 1));
         }
 
-		long end = System.currentTimeMillis();
+    	long end = System.currentTimeMillis();
 
-		System.out.println("Test took " + ((float)(end - start) / 1000) + " seconds.");
+    	System.out.println("Test took " + ((float)(end - start) / 1000) + " seconds.");
     }
     
     private static int f(int a, int b) {
-        if( (a == 1) || (b == 1) )
-            return 2;
-    
+        if (a == 1 && b == 1)
+    		   return 1;
+
+        if(a == 1)
+            return 1 + f(a, b-1);
+
+        if(b == 1)
+            return 1 + f(a-1, b);
+   
         return (f(a-1, b) + f(a, b-1));
     }
 }
