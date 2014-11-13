fed37257a10b9b46806c506d1fb14fa995ced4a4
David Young <david@nospam.fake>
2014-11-12 19:26:05 -0800

    Adding .gitignore and a single test. Will add more in the future.

---
+++ AlgoTest.java
@@ -0,0 +1,27 @@
+/**
+ * Directions:
+ * 1) Run this as is and see how long it takes.
+ * 2) Make changes to the body of "f" to make it faster without changing the output of the program.
+ */
+public class AlgoTest {
+    public static void main(String[] args) {
+		long start = System.currentTimeMillis();
+
+        for(int num = 1; num < 20; num++) {
+            System.out.println("f(" + num + ", " + num + ") = " + f(num, num));
+            System.out.println("f(" + num + ", " + (num+1) + ") = " + f(num, num + 1));
+        }
+
+		long end = System.currentTimeMillis();
+
+		System.out.println("Test took " + ((float)(end - start) / 1000) + " seconds.");
+    }
+    
+    private static int f(int a, int b) {
+        if( (a == 1) || (b == 1) )
+            return 2;
+    
+        return (f(a-1, b) + f(a, b-1));
+    }
+}
+
